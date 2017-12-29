32110ea1929c3eaf67d68a957fb91aa450b05026
ohad <olevaretz@gmail.com>
2017-12-29 13:55:09 -0500

    first commit, model is NOT good, just testing to run on FLOYD

--- K_Statoil.py
+++ K_Statoil.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import random
 from sklearn.model_selection import train_test_split
 import keras
 import matplotlib.pyplot as plt
@@ -10,6 +11,66 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from keras.layers.normalization import BatchNormalization
 from keras import backend as K
 from keras.optimizers import Adam
+
+def clean_img (img):
+    img = np.array(img)
+    mn, mx = img.min(), img.max()
+    img = (img - mn) / (mx - mn)
+    img[img < (img.mean()+img.std()*1)] = 0
+    return img.reshape((75, 75))
+
+def clean_from_img (img1, img2):
+    img2 = np.array(img2)
+    img2[img1.reshape(75*75) == 0] = 0
+    return img2.reshape(75, 75)
+
+def rotndup_img (img):
+    img90 = np.array(list(zip(*img[::-1])))
+    img180 = np.array(list(zip(*img90[::-1])))
+    img270 = np.array(list(zip(*img180[::-1])))
+    return [img, img90, img180, img270]
+
+def deep_clean (bd1):
+    # begins as a regular cleaning of noise anything below mean + 1 std will zero
+    bd1 = np.array(bd1)
+    mn, mx = bd1.min(), bd1.max()
+    bd1 = (bd1 - mn) / (mx - mn)
+    bd1[bd1 < (bd1.mean() + bd1.std() * 1)] = 0
+    bd1 = bd1.reshape(75, 75)
+
+    # defining an array to hold "remember" our object pattern
+    part_of = np.zeros((75, 75))
+
+    # we are searching waterfall style only upside down, and so we extract the peaks and work our way down
+    # until we reach the previous pixel marked as 0. this will define the major structurs in the image.
+    # All the rest is squashed.
+    top95 = np.percentile(bd1, 99.5)
+    row, col = np.where(bd1 > top95)
+
+    lst = [[-1, 1], [0, 1], [1, 1], [-1, 0], [1, 0], [-1, -1], [0, -1], [-1, -1]]
+
+    for i in range(len(row)):
+        x = row[i]
+        y = col[i]
+        cell2chk = [[x, y]]
+        j = 0
+        macktub = True
+        while macktub:
+            x, y = cell2chk[j]
+            part_of[x, y] = 1
+            for sx, sy in lst:
+                if (x+sx) < 75 and (y+sy) < 75 and (x+sx) >= 0 and (y+sy) >= 0:
+                    if bd1[x + sx, y + sy] != 0 and part_of[x + sx, y + sy] == 0:
+                        part_of[x + sx, y + sy] = 1
+                        cell2chk.append([x + sx, y + sy])
+                    elif bd1[x + sx, y + sy] == 0:
+                        part_of[x + sx, y + sy] = -1
+            j += 1
+            if j + 1 >= len(cell2chk):
+                macktub = False
+
+    bd1[part_of == 0] = 0
+    return bd1
 
 
 def getModel():
@@ -24,12 +85,11 @@ def getModel():
 
     # CNN 2
     model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-    model.add(Conv2D(128, kernel_size=(5, 5), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
     model.add(Dropout(0.2))
 
     # CNN 3
-    model.add(Conv2D(128, kernel_size=(5, 5), activation='relu'))
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
     model.add(Dropout(0.2))
 
@@ -58,26 +118,17 @@ def getModel():
 
     return model
 
-
+# load the training data
 train = pd.read_json('/Users/ohad/Google_Drive/DS_projects/K_Statoil/Train/train.json')
 train['inc_angle'] = pd.to_numeric(train['inc_angle'], errors='coerce')
 
 # option1 fill the images with nan for inc_angle with mean of angles
-train['inc_angle'] = train['inc_angle'].fillna(train['inc_angle'].mean())
+#train['inc_angle'] = train['inc_angle'].fillna(train['inc_angle'].mean())
 
 # option2 drop images with nan
-# train = train.dropna(axis=0, how='any')
+train = train.dropna(axis=0, how='any')
 
 print('train has been read')
-
-"""bnd1 = np.array(train.band_1[0]).reshape(75,75)
-x = range(75)
-y = bnd1[:, x]
-plt.hist3d(, bins=50)
-plt.imshow(clean_img(bnd1))"""
-
-
-
 
 
 # split icebergs from ships
@@ -90,45 +141,14 @@ ships = train[train.is_iceberg == 0].sample(n=4, random_state=38)
 # creating a df for training the model
 df = pd.concat([icebergs, ships])"""
 
-# Defining anything under mean+std as noise and reduce it the minimum value
-
-def clean_img (img):
-    img = np.array(img)
-    mn, mx = img.min(), img.max()
-    img = (img - mn) / (mx - mn)
-    img[img < (img.mean()+img.std()*1)] = 0
-    return img.reshape((75, 75))
-
-def clean_from_img (img1, img2):
-    img2 = np.array(img2)
-    img2[img1.reshape(75*75) == 0] = 0
-    return img2.reshape(75, 75)
-
-def rotndup_img (img):
-    img90 = np.array(list(zip(*img[::-1])))
-    img180 = np.array(list(zip(*img90[::-1])))
-    img270 = np.array(list(zip(*img180[::-1])))
-    return [img, img90, img180, img270]
-
-def deep_clean (bnd1, bnd2):
-    bnd1 = np.array(bnd1)
-    mn, mx = bnd1.min(), bnd1.max()
-    bnd1 = (bnd1 - mn) / (mx - mn)
-    bnd1[bnd1 < (bnd1.mean()+bnd1.std()*1)] = 0
-
-
-
-
-#bnd1_ly = train['band_1'].apply(clean_img)
-#bnd2_ly = train['band_2'].apply(clean_img)
-
 # input image dimensions
-img_rows, img_cols, layers = 75, 75, 2
+img_rows, img_cols, layers = 75, 75, 1
 
 
 # Creating more test cases
 print('creating more test cases')
 
+avg = 0
 enh_ly = []
 bnd1_ly = []
 bnd2_ly = []
@@ -139,20 +159,31 @@ y = []
 for i in train.index:
     print(i)
 
-    # cleaned layer band_1 HH (cHH)
+    """"# cleaned layer band_1 HH (cHH)
     bnd1 = clean_img(train.at[i, 'band_1'])
     bnd1_ly.extend(rotndup_img(bnd1))
 
     # cleaned layer band_2 HV (in the future will cleaned based on the zero of HH) - cHV
     bnd2 = clean_img(train.at[i, 'band_2'])
+    bnd2_ly.extend((rotndup_img(bnd2)))"""
+
+    # cleaned layer band_1 HH (cHH)
+    bnd1 = deep_clean(train.at[i, 'band_1'])
+    bnd2 = np.array(train.at[i, 'band_2']).reshape(75, 75)
+    bnd2[bnd1 == 0] = 0
+    bnd1_ly.extend(rotndup_img(bnd1))
+
+    # cleaned layer band_2 HV (in the future will cleaned based on the zero of HH) - cHV
     bnd2_ly.extend((rotndup_img(bnd2)))
+
 
     # Enhanced layer of cHH + cHV
     enh_img = bnd1 + bnd2
+    avg = avg + np.mean(enh_img, axis = (0, 1))
     enh_ly.extend(rotndup_img(enh_img))
 
     # layer containing the angle
-    inc_ang[bnd1 != 0] = train.at[i, 'inc_angle']/2000
+    inc_ang[bnd1 != 0] = train.at[i, 'inc_angle']
     inc_ang[bnd1 == 0] = 0
     inc_ang_ly.extend([inc_ang, inc_ang, inc_ang, inc_ang])
 
@@ -168,16 +199,17 @@ for i in train.index:
 
     y.extend([ice, ice, ice, ice])
     """
+
     """
-    # layer NOT containing the angle
-    X.append(np.dstack((bnd1_ly[0], bnd2_ly[0], enh_ly[0])))
-    X.append(np.dstack((bnd1_ly[1], bnd2_ly[1], enh_ly[1])))
-    X.append(np.dstack((bnd1_ly[2], bnd2_ly[2], enh_ly[2])))
-    X.append(np.dstack((bnd1_ly[3], bnd2_ly[3], enh_ly[3])))
+    # layer NOT containing the angle and the enh_ly
+    X.append(np.dstack((bnd1_ly[0], bnd2_ly[0])))
+    X.append(np.dstack((bnd1_ly[1], bnd2_ly[1])))
+    X.append(np.dstack((bnd1_ly[2], bnd2_ly[2])))
+    X.append(np.dstack((bnd1_ly[3], bnd2_ly[3])))
     
     y.extend([ice, ice, ice, ice])
-    
     """
+
     """
     # layer bnd1, bnd2, angle
     X.append(np.dstack((bnd1_ly[0], bnd2_ly[0], inc_ang_ly[0])))
@@ -187,13 +219,27 @@ for i in train.index:
 
     y.extend([ice, ice, ice, ice])
     """
+    """
     # layer containing ONLY enh + angle with no duplication of the data
     X.append(np.dstack((enh_img, inc_ang)))
 
     y.extend([ice])
+    """
 
+    # layer containing ONLY enh with NO duplication of the data
+    X.append(np.dstack(enh_img))
+    
+    y.extend([ice])
 
+    """
+    # layer containing ONLY enh with duplication of the data
+    X.append(np.dstack(enh_ly[0]))
+    X.append(np.dstack(enh_ly[1]))
+    X.append(np.dstack(enh_ly[2]))
+    X.append(np.dstack(enh_ly[3]))
 
+    y.extend([ice, ice, ice, ice])
+    """
 """
 what I want to do here is create 5 layers:
     1. Cleaned layer band_1
@@ -258,10 +304,13 @@ plt.show()
 """
 
 X = np.array(X)
+avg = avg/len(X)
+X = X - avg
+random.shuffle(X)
 y = np.array(y)
 
 # split train and validate data
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
 # configuring the input_shape and the x, y to the tensor flow structure
 if K.image_data_format() == 'channels_first':
@@ -283,7 +332,7 @@ print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
 # Setup model parameters
-batch_size = 200
+batch_size = 100
 epochs = 50
 earlyStopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='min')
 mcp_save = ModelCheckpoint('.mdl_wts.hdf5', save_best_only=True, monitor='val_loss', mode='min')
@@ -305,4 +354,219 @@ model.load_weights(filepath = '.mdl_wts.hdf5')
 score = model.evaluate(x_test, y_test, verbose=1)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+
+
+
+
+# Draft:
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+rnd_hist = [685]
+
+rnd = np.random.randint(len(train))
+
+########################
+
+
+bd1 = train.band_1[rnd]
+bd2 = np.array(train.band_2[rnd]).reshape(75, 75)
+bd1 = np.array(bd1)
+
+plt.subplot(231)
+plt.title('Band_1 Raw')
+raw = bd1.reshape(75, 75)
+plt.imshow(raw)
+mn, mx = bd1.min(), bd1.max()
+bd1 = (bd1 - mn) / (mx - mn)
+bd1[bd1 < (bd1.mean() + bd1.std() * 1)] = 0
+
+part_of = np.zeros((75, 75))
+peaks = np.zeros((75, 75))
+chk = np.zeros((75, 75))
+bd1 = bd1.reshape(75, 75)
+
+# standard cleaning
+plt.subplot(235)
+plt.imshow(bd1)
+plt.title('std clean')
+
+top95 = np.percentile(bd1, 99.5)
+row, col = np.where(bd1 > top95)
+
+# defining the border of a cell
+lst = [[-1, 1], [0, 1], [1, 1], [-1, 0], [1, 0], [-1, -1], [0, -1], [-1, -1]]
+
+
+for i in range(len(row)):
+    x = row[i]
+    y = col[i]
+    print('{} out of {}'.format(i, len(row)))
+    cell2chk = [[x, y]]
+    j = 0
+    macktub = True
+    while macktub:
+        x, y = cell2chk[j]
+        part_of[x, y] = 1
+        for sx, sy in lst:
+            if bd1[x+sx, y+sy] != 0 and part_of[x+sx, y+sy] == 0:
+                part_of[x+sx, y+sy] = 1
+                cell2chk.append([x+sx, y+sy])
+            elif bd1[x+sx, y+sy] == 0:
+                part_of[x+sx, y+sy] = -1
+        j += 1
+        if j+1 >= len(cell2chk):
+            macktub = False
+
+
+plt.subplot(232)
+bd1[part_of == -1] = 0
+bd1[part_of == 0] = 0
+plt.imshow(bd1)
+plt.title('deep clean')
+
+plt.subplot(233)
+for peak in range(len(row)):
+    peaks[row[peak], col[peak]] = 20
+plt.imshow(peaks)
+plt.title('Peaks')
+
+plt.subplot(236)
+plt.imshow(part_of)
+plt.title('Part_of')
+
+
+#######################################
+
+bd1 = train.band_1[rnd]
+bd2 = np.array(train.band_2[rnd]).reshape(75, 75)
+bd1 = np.array(bd1)
+
+plt.subplot(231)
+plt.title('Band_1 Raw')
+raw = bd1.reshape(75, 75)
+plt.imshow(raw)
+mn, mx = bd1.min(), bd1.max()
+bd1 = (bd1 - mn) / (mx - mn)
+bd1[bd1 < (bd1.mean() + bd1.std() * 1)] = 0
+part_of = np.zeros((75, 75))
+where_i_have_been = np.zeros((75, 75))
+peaks = np.zeros((75, 75))
+bd1 = bd1.reshape(75, 75)
+
+plt.subplot(235)
+reg_cl = bd1
+plt.title('Reg_Clean')
+plt.imshow(reg_cl)
+
+plt.subplot(232)
+top95 = np.percentile(bd1, 99.5)
+row, col = np.where(bd1 > top95)
+for peak in range(len(row)):
+    chk_360(bd1, row[peak], col[peak])
+
+bd1[part_of == -1] = 0
+bd1[part_of == 0] = 0
+plt.imshow(bd1)
+plt.title('deep clean')
+
+plt.subplot(233)
+for peak in range(len(row)):
+    peaks[row[peak], col[peak]] = 20
+plt.imshow(peaks)
+plt.title('Peaks')
+
+plt.subplot(236)
+plt.imshow(part_of)
+plt.title('Part_of')
+
+plt.subplot(234)
+plt.imshow(where_i_have_been)
+plt.title('where_i_have_been')
+plt.show()
+
+print('func calls:', chk_calls)
+print('')
+
+from skimage.filters import sobel
+from skimage.morphology import watershed
+from scipy import ndimage as ndi
+
+markers = np.zeros_like(reg_cl)
+markers[reg_cl > 0.4] = 2
+markers[reg_cl < 0.3] = 1
+
+elevation_map = sobel(reg_cl)
+segmentation = watershed(elevation_map, markers)
+
+segmentation = ndi.binary_fill_holes(segmentation - 1)
+labeled_obj, _ = ndi.label(segmentation)
+"""
+
+
+"""
+rnd = 1021
+rnd = np.random.randint(0, len(train)-1)
+
+bd1 = train.at[rnd, 'band_1']
+raw = np.array(bd1).reshape(75, 75)
+
+
+bd1 = np.array(bd1)
+mn, mx = bd1.min(), bd1.max()
+bd1 = (bd1 - mn) / (mx - mn)
+bd1[bd1 < (bd1.mean() + bd1.std() * 1)] = 0
+clean = bd1.reshape(75, 75)
+bd1 = bd1.reshape(75, 75)
+
+# defining an array to hold "remember" our object pattern
+part_of = np.zeros((75, 75))
+
+# we are searching waterfall style only upside down, an so we extract the peaks and work our way down.
+top95 = np.percentile(bd1, 99.5)
+row, col = np.where(bd1 > top95)
+
+lst = [[-1, 1], [0, 1], [1, 1], [-1, 0], [1, 0], [-1, -1], [0, -1], [-1, -1]]
+
+for i in range(len(row)):
+    x = row[i]
+    y = col[i]
+    cell2chk = [[x, y]]
+    j = 0
+    macktub = True
+    while macktub:
+        x, y = cell2chk[j]
+        part_of[x, y] = 1
+        for sx, sy in lst:
+            if (x + sx) < 75 and (y + sy) < 75 and (x + sx) >= 0 and (y + sy) >= 0:
+                if bd1[x + sx, y + sy] != 0 and part_of[x + sx, y + sy] == 0:
+                    part_of[x + sx, y + sy] = 1
+                    cell2chk.append([x + sx, y + sy])
+                elif bd1[x + sx, y + sy] == 0:
+                    part_of[x + sx, y + sy] = -1
+        j += 1
+        if j + 1 >= len(cell2chk):
+            macktub = False
+
+bd1[part_of == 0] = 0
+
+plt.subplot(232)
+plt.title('ice: {}, angle: {}'.format(train.at[rnd, 'is_iceberg'], train.at[rnd, 'inc_angle'] ))
+
+
+plt.subplot(234)
+plt.imshow(raw)
+plt.title('Raw')
+
+plt.subplot(235)
+plt.imshow(clean)
+plt.title('Reg Clean')
+
+plt.subplot(236)
+plt.imshow(bd1)
+plt.title('Deep Clean')
+
+"""
+
 
